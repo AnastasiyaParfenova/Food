b9b6f1efba573304e4452c834112e5d6fa0c58cb
Anastasiya <parfenova.anastasiya.98@yandex.ru>
2024-01-10 14:37:34 +0500

    fetch API

--- js/script.js
+++ js/script.js
@@ -233,30 +233,36 @@ window.addEventListener('DOMContentLoaded', function() {
                 margin: 0 auto;
             `;
             form.insertAdjacentElement('afterend', statusMessage);
-        
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            
             const formData = new FormData(form);
 
             const object = {};
             formData.forEach(function(value, key){
                 object[key] = value;
             });
-            const json = JSON.stringify(object);
 
-            request.send(json);
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json' 
+                },
+                body: JSON.stringify(object)
+            })
+            
+            .then(data => data.text())
+            
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+        
+            }).catch(() => {
+                showThanksModal(message.failure);
 
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    statusMessage.remove();
-                    form.reset();
-                } else {
-                    showThanksModal(message.failure);
-                }
-            });
+            }).finally(() => {
+                form.reset();
+            })
+
         });
     }
 
@@ -282,4 +288,18 @@ window.addEventListener('DOMContentLoaded', function() {
             closeModal();
         }, 4000);
     }
+
+    //POST, fetch API
+//     fetch('https://jsonplaceholder.typicode.com/posts',
+//     {
+//         method: "POST",
+//         body: JSON.stringify({name: 'Alex'}),
+//         headers: {
+//             'Content-type': 'application/json'
+//         }
+//     })
+//       .then(response => response.json())
+//       .then(json => console.log(json));
 });
+
+
